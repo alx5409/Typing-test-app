59a65bcd2e96080da23edfbe6b01a3b2c5b68e7a
Alex <alx5409@gmail.com>
2025-10-21 12:41:26 +0200

    Add documentation in test

--- src/models/test.go
+++ src/models/test.go
@@ -2,14 +2,15 @@ package models
 
 import "time"
 
+// TypeTest represents a typing test performed by the user
 type TypeTest struct {
-	Id           int64
-	TextToType   string
-	TextTyped    string
-	StartTime    time.Time
-	EndTime      time.Time
-	NumberErrors int
-	TypingSpeed  float32
+	Id           int64     // Unique identifier for the test
+	TextToType   string    // Text the user must type
+	TextTyped    string    // Text entered by the user
+	StartTime    time.Time // Time when the test starts
+	EndTime      time.Time // Time when the test ends
+	NumberErrors int       // Numbers of errors made
+	TypingSpeed  float32   // Typing speed of the test
 }
 
 func (t *TypeTest) ComputeAccuracy() float32 {
@@ -19,11 +20,14 @@ func (t *TypeTest) ComputeAccuracy() float32 {
 	return 1 - float32(t.NumberErrors)/float32(len(t.TextTyped))
 }
 
+// Computes the total test time in seconds as float32.
 func (t *TypeTest) ComputeTestTime() float32 {
 	duration := t.EndTime.Sub(t.StartTime).Seconds()
 	return float32(duration)
 }
 
+// Computes the user's keystrokes per minutes (PPM).
+// Returns 0 if the duration is 0.
 func (t *TypeTest) ComputeTypingSpeedPPM() float32 {
 	duration := t.ComputeTestTime()
 	if duration == 0 {
@@ -32,6 +36,9 @@ func (t *TypeTest) ComputeTypingSpeedPPM() float32 {
 	return (float32(len(t.TextTyped)) / duration) * 60
 }
 
+// Computes the user's words per minute (WPM) counting words
+// as sequences separated by spaces.
+// Returns 0 i fthe duration is zero.
 func (t *TypeTest) ComputeTypingSpeedWPM() float32 {
 	duration := t.ComputeTestTime()
 	if duration == 0 {
