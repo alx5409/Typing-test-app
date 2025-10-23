cb662b79e1f85ec267dd57ed88b5a3cf495fc657
Alex <alx5409@gmail.com>
2025-10-23 09:40:11 +0200

    Add random text generator - Use Random Word API

--- src/utils/helpers.go
+++ src/utils/helpers.go
@@ -1,13 +1,37 @@
 package utils
 
-import {
+import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-}
-API_URL = "https://random-word-api.herokuapp.com/word"
+)
+
+const API_URL = "https://random-word-api.herokuapp.com/word"
+
 // Generates random text for the typing test
 func GenerateRandomText() string {
 	resp, err := http.Get(API_URL)
-	return ""
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+	var words []string
+	if err := json.Unmarshal(body, &words); err != nil {
+		return ""
+	}
+
+	// Joins the words into a single string separated by spaces
+	text := ""
+	for i, word := range words {
+		if i > 0 {
+			text += " "
+		}
+		text += word
+	}
+	return text
 }
