95fda4686401f2ba48d187867ba6d8e693a85ae3
Alex <alx5409@gmail.com>
2025-10-24 23:37:38 +0200

    Improve doc in funcions of handlers

--- src/handlers/typing.go
+++ src/handlers/typing.go
@@ -7,7 +7,8 @@ import (
 	"time"
 )
 
-// StartTest initializes a new typing test, generates the text, and sets the start time.
+// StartTest initializes a new typing test, generates the text to type, and sets the start time.
+// It prints the text that the user should type and returns a pointer to the initialized TypeTest struct.
 func StartTest() *models.TypeTest {
 	test := &models.TypeTest{
 		TextToType: utils.GenerateRandomText(),
@@ -18,7 +19,8 @@ func StartTest() *models.TypeTest {
 	return test
 }
 
-// EndTest finalizes the typing test, sets the end time, and processes the results.
+// EndTest finalizes the typing test by setting the end time, saving the user's input,
+// counting errors, and calculating the typing speed (WPM).
 func EndTest(test *models.TypeTest, typedText string) {
 	test.TextTyped = typedText
 	test.EndTime = time.Now()
@@ -26,11 +28,12 @@ func EndTest(test *models.TypeTest, typedText string) {
 	test.TypingSpeed = test.ComputeTypingSpeedWPM()
 }
 
-// ShowResults displays the results of the typing test.
+// ShowResults displays the results of the typing test, including accuracy,
+// keystrokes per minute (PPM), words per minute (WPM), number of errors, and test duration.
 func ShowResults(test *models.TypeTest) {
-    fmt.Printf("Accuracy: %.2f%%\n", test.ComputeAccuracy()*100)
-    fmt.Printf("Keystrokes per minute (PPM): %.2f\n", test.ComputeTypingSpeedPPM())
-    fmt.Printf("Words per minute (WPM): %.2f\n", test.ComputeTypingSpeedWPM())
-    fmt.Printf("Errors: %d\n", test.NumberErrors)
-    fmt.Printf("Test duration: %.2f seconds\n", test.ComputeTestTime())
+	fmt.Printf("Accuracy: %.2f%%\n", test.ComputeAccuracy()*100)
+	fmt.Printf("Keystrokes per minute (PPM): %.2f\n", test.ComputeTypingSpeedPPM())
+	fmt.Printf("Words per minute (WPM): %.2f\n", test.ComputeTypingSpeedWPM())
+	fmt.Printf("Errors: %d\n", test.NumberErrors)
+	fmt.Printf("Test duration: %.2f seconds\n", test.ComputeTestTime())
 }
